de7f5193b599908edb9326cc9d0ccbccca00d902
PUTTAIAH <kzq936@9801a7a5a213.cof.ds.capitalone.com>
2016-11-26 12:06:53 -0500

    Home Page Chart changes

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,12 +1,14 @@
 'use strict';
-define(['ojs/ojcore', 'knockout', 'jquery', 'viewModels/service/dataService','ojs/ojmodule','ojs/ojlistview', 'ojs/ojarraytabledatasource'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'viewModels/service/dataService','ojs/ojknockout','ojs/ojmodule','ojs/ojlistview', 
+    'ojs/ojbutton','ojs/ojarraytabledatasource', 'ojs/ojchart', 'ojs/ojgauge', 'ojs/ojselectcombobox'],
     function(oj, ko, $, service)
     {   
-    	function DashboardViewModel(){
+    	function DashboardViewModel() {
     		//self.pendingdecisionlist = ko.observableArray([]);
     		//self.activitylist = ko.observableArray([]);
             self.pendingdecisions = ko.observable();
             self.activities=ko.observable();
+            self.wish=ko.observable();
 
     		var header = {
     			"Access-Control-Allow-Origin": "*",
@@ -24,7 +26,40 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'viewModels/service/dataService','oj
                 //self.activitylist(response);
                 self.activities(new oj.ArrayTableDataSource(response));
             });
-    	}
+
+             service.fetch('http://54.158.170.219/RESTEasy/rest/service/users/1234/wishes',header).then(function(response) {
+                //self.activitylist(response);
+                self.activities(new oj.ArrayTableDataSource(response));
+            });
+
+            // Chart Data Maping
+            self.seriesValues = ko.observableArray();
+            self.groupValues = ko.observableArray();
+
+            // Load Chart Data
+            service.fetch('js/data/chart.json',header).then(function(response) {
+                var grp = response['group'];
+                self.groupValues(grp);
+                var data = [    
+                                { name:'Total Asset Value', items: response['total_asset'] },
+                                { name:'Desired Value', items: response['desired'] },
+                                
+                                
+                                { name:'Liquid Value', items: response['liquid'] }
+                                
+                                
+                            ];
+                self.seriesValues(data);
+            });
+
+            var dateConverterFactory = oj.Validation.converterFactory('datetime');
+            var option = {formatStyle: 'date', isoStrFormat: 'auto'};
+            self.xAxisConverter = ko.observable(dateConverterFactory.createConverter(option));
+
+            var numberConverterFactory = oj.Validation.converterFactory('number');
+            self.yAxisConverter = ko.observable(numberConverterFactory.createConverter({style: 'currency', currency: 'USD', 
+                                                decimalFormat:'short', maximumFractionDigits:0}));
+        }
         return DashboardViewModel;  	
     }
 );
