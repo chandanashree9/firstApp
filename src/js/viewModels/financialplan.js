5d18253220c7a5b69ee9b40c5e39c66fb75b58e2
kzq936 <vinaykumar.puttaiah@capitalone.com>
2016-12-30 09:50:22 -0500

    Fixed default display of Financial Plan View Chart

--- src/js/viewModels/financialplan.js
+++ src/js/viewModels/financialplan.js
@@ -21,9 +21,21 @@ function(oj, ko, $, service, numberconvertor) {
 		}
 
         // Start - Impacts Desires
+        self.planviews = ko.observableArray([]);
+        var planViewNames = [];
+
+        // first time on page load
+        var desireslist = [];
+        var selecteddesireId = '';
+
         self.impactdesires=ko.observableArray([]);
         service.fetch(planview_url,header).then(function(response) {
-            self.impactdesires(response.sort(prioritySort));
+            desireslist = response.sort(prioritySort);
+            self.impactdesires(desireslist);
+
+            selecteddesireId = desireslist[0].id;
+            var planviewlist = computePlanView(desireslist,selecteddesireId, planViewNames);
+            self.planviews(planviewlist);
         });
         // End - Impacts Desires
 
@@ -35,73 +47,17 @@ function(oj, ko, $, service, numberconvertor) {
         self.yAxisConverter = ko.observable(numberconvertor.currencyConverter());
         // End - Chart Display Contents
 
-        self.planviews = ko.observableArray([]);
-        var palnViewNames = [];
-
+        self.dataCursorPositionValue = ko.observable(null);
         self.GetPlanViewName = {
             format: function(value) {
-                return palnViewNames[value];
+                return planViewNames[value];
             }
         };
 
-        self.dataCursorPositionValue = ko.observable(null);
-
-        self.optionChangeHandler = function(event, data){
-            console.log(event.target.id);            
-            var planviewlist = [];
-            var preparelist = [];
-            var desireslist = self.impactdesires._latestValue;
-
-            var selecteddesire = {};
-            var list1 = [];
-            var list2 = [];
-
-            for(var i = 0; i < desireslist.length; i++){
-                if(desireslist[i].id == event.target.id) {
-                    selecteddesire = desireslist[i];
-                    break;
-                }
-            }
-
-            for(var i = 0; i < desireslist.length; i++){
-                var desires = desireslist[i];  
-
-                var d = new Date().getTime();
-                if(selecteddesire.expense_Date) {
-                    d = new Date(selecteddesire.expense_Date).getTime();
-                }
-                var d1 = new Date().getTime();
-                if(desires.expense_Date) {
-                    d1 = new Date(desires.expense_Date).getTime();
-                }
-
-                if(d < d1 ){
-                   list1.push(desires);
-                } else if(selecteddesire.id != desires.id) {
-                    list2.push(desires);
-                }           
-            }
-
-            list1.sort();
-            list2.sort();
-
-            if(list1.length > 3){
-               list1 = list1.slice(-3); 
-            }
-            list1.push(selecteddesire);
-            if(list2.length > 2) {
-                list2 = list2.slice(0,2);
-            }
-            preparelist = list1.concat(list2);
-
-            var count = 0;
-            for(var i = preparelist.length -1 ; i >= 0; i--){
-                var desire = preparelist[i];  
-                if(desire.target_date) {
-                    palnViewNames[count++] = desire.title;
-                    planviewlist.push({items:[{y:i, x:new Date(desire.target_date).getTime()}],name:desire.title});
-                }                
-            }
+        self.optionChangeHandler = function(event, data){   
+            desireslist = self.impactdesires._latestValue;       
+            selecteddesireId = event.target.id;
+            var planviewlist = computePlanView(desireslist,selecteddesireId, planViewNames);
             self.planviews(planviewlist);
         }
 
@@ -141,6 +97,63 @@ function(oj, ko, $, service, numberconvertor) {
             return 1;
         }        
         return 0;
+    }
+
+    function computePlanView(desireslist, selectedDesireId, planViewNames){
+        var planviewlist = [];
+        var preparelist = [];       
+
+        var selecteddesire = {};
+        var list1 = [];
+        var list2 = [];
+
+        for(var i = 0; i < desireslist.length; i++){
+            if(desireslist[i].id == selectedDesireId) {
+                selecteddesire = desireslist[i];
+                break;
+            }
+        }
+
+        for(var i = 0; i < desireslist.length; i++){
+            var desires = desireslist[i];  
+
+            var d = new Date().getTime();
+            if(selecteddesire.expense_Date) {
+                d = new Date(selecteddesire.expense_Date).getTime();
+            }
+            var d1 = new Date().getTime();
+            if(desires.expense_Date) {
+                d1 = new Date(desires.expense_Date).getTime();
+            }
+
+            if(d < d1 ){
+               list1.push(desires);
+            } else if(selecteddesire.id != desires.id) {
+                list2.push(desires);
+            }           
+        }
+
+        list1.sort();
+        list2.sort();
+
+        if(list1.length > 3){
+           list1 = list1.slice(-2); 
+        }
+        list1.push(selecteddesire);
+        if(list2.length > 2) {
+            list2 = list2.slice(0,2);
+        }
+        preparelist = list1.concat(list2);
+
+        var count = 0;
+        for(var i = preparelist.length -1 ; i >= 0; i--){
+            var desire = preparelist[i];  
+            if(desire.target_date) {
+                planViewNames[count++] = desire.title;
+                planviewlist.push({items:[{y:i, x:new Date(desire.target_date).getTime()}],name:desire.title});
+            }                
+        }
+        return planviewlist;
     }
 
     return FinancialViewModel;
